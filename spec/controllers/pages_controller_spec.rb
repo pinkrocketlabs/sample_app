fb5e9eff7c9cd8d1a0ad014ab819bf2231a2033a
Kevin <pinkrocketlabs@gmail.com>
2011-06-07 22:15:19 +0800

    Added layout link

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -27,20 +27,6 @@ describe PagesController do
 
   describe "GET 'contact'" do
     it "should be successful" do
-      get 'contact'
-      response.should be_success
-    end
-    
-    it "should have the right title" do
-      get 'contact'
-      response.should have_selector("title",
-                        :content => "#{@base_title} | Contact")
-    end
-    
-  end
-  
-  describe "GET 'about'" do
-    it "should be successful" do
       get 'about'
       response.should be_success
     end
@@ -49,6 +35,20 @@ describe PagesController do
       get 'about'
       response.should have_selector("title",
                         :content => "#{@base_title} | About")
+    end
+    
+  end
+  
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+                        :content => "#{@base_title} | Help")
     end
   end
   
